e47e807b6a4f2aa95a348cd4fa5fc3adf10a6594
jaraction <a.jaramilloc2@uniandes.edu.co>
2024-03-10 23:25:05 -0500

    m

--- p1.py
+++ p1.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(100000000)
+
 def encontrar_i(T):
     for i in range(len(T) - 1, -1, -1):
         if T[i] < T[0] + 1:
@@ -15,6 +18,11 @@ soluciones = []
 def F(T, i=None, movs=0):
     if i is None:
         i = encontrar_i(T)
+
+    x = encontrar_x(T, i)
+
+    if x < i - 1:
+        new_moves = (i - 1)-x
     
     if i == -1:
         soluciones.append(movs)
@@ -24,32 +32,20 @@ def F(T, i=None, movs=0):
         return float('inf')
     
     elif i == 0:
-        return F([T[0] + 1, T[1] - 1] + T[2:], None, movs + 1)
+        return F([T[0] + 1, T[1] - 1] + T[2:], i, movs + 1)
     
-    x = encontrar_x(T, i)
+    elif x == -1:
+        return F(T[:i] + [T[i] + 1, T[i + 1] - 1] + T[i + 2:], i, movs + 1)
     
-    if x == -1: # No se encontró x, no se pueden hacer movimientos
-        return float('inf')
+    elif i > 0  and F(T[:i] + [T[i] + 1, T[i + 1] - 1] + T[i + 2:], i, movs + 1) <= F(T[:x] + [T[x]-1] + T[x+1:i] + [T[i]+1] + T[i+1:], i, movs + new_moves):
+        return F(T[:i] + [T[i] + 1, T[i + 1] - 1] + T[i + 2:], i, movs + 1)
     
-    new_moves = 2 * (i - x - 1) if x < i - 1 else 1
-    
-    # Verifica si i + 1 está dentro del rango antes de proceder
-    if i + 1 < len(T):
-        op1 = F(T[:i] + [T[i] + 1, T[i + 1] - 1] + T[i + 2:], None, movs + 1)
-    
-    if x < i - 1:
-        op2 = F(T[:x] + [T[x]-1] + T[x+1:i] + [T[i]+1] + T[i+1:], None, movs + new_moves)
     else:
-        op2 = float('inf')  # O alguna otra lógica que se adecue al problema
-    
-    if op1 <= op2:
-        return op1
-    else:
-        return op2
+        return F(T[:x] + [T[x]-1] + T[x+1:i] + [T[i]+1] + T[i+1:], i, movs + new_moves)
 
 # Para utilizar la función, proporciona la lista inicial T y llama a F(T)
 # Ejemplo:
 # T = [cantidad de fichas en cada torre]
 # print(F(T))
 
-print(F([3,2,2,4]))
+print(F([3,2]))

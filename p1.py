3ccbdd67eb9ccfe6d8220f6f2cdc670ee66ef17a
jaraction <a.jaramilloc2@uniandes.edu.co>
2024-03-10 20:30:15 -0500

    init

---
+++ p1.py
@@ -0,0 +1,10 @@
+def main():
+    cases = int(input().strip())
+
+    for _ in range(cases):
+
+        inputs = list(map(int, input().strip().split()))
+        print(inputs)
+
+if __name__ == "__main__":
+    main()

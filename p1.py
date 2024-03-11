073c7298adf1fa920d13d54ae5c46292af09db59
jaraction <a.jaramilloc2@uniandes.edu.co>
2024-03-10 20:44:13 -0500

    xd

--- p1.py
+++ p1.py
@@ -1,3 +1,13 @@
+new_moves = 0
+
+def F(towers, i, moves):
+
+    if i == 0:
+        return F([towers[0]+1, towers[1],-1] + towers[2:], i, moves+1)
+    elif i > 0 and F(towers[:i]+ [towers[i]+1, towers[i+1]-1] + towers[i+2:], i, moves + 1):
+        return F(towers[:i]+ [towers[i]+1, towers[i+1]-1] + towers[i+2:], i, moves + 1)
+
+
 def main():
     cases = int(input().strip())
 
